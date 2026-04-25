e51f76aae04c47462daf1c92634cb6f9902ca487
hiron <hironkanishka97@gmail.com>
2026-04-25 12:06:47 +0530

    report controller error fixed

--- ServerApp/src/main/java/lk/newdayproducts/report/ReportController.java
+++ ServerApp/src/main/java/lk/newdayproducts/report/ReportController.java
@@ -1,24 +1,23 @@
 package lk.newdayproducts.report;
-import lk.newdayproducts.dao.MaterialcategoryDao;
-import lk.newdayproducts.entity.Materialcategory;
+
+import lk.newdayproducts.entity.Charge;
 import lk.newdayproducts.report.dao.CountByMaterialCategoryDao;
 import lk.newdayproducts.report.entity.CountByMaterialCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/reports")
 public class ReportController {
 
-    @Autowired
-    private CountByMaterialCategoryDao countbymaterialcategorydao;
-    @GetMapping(path ="/CountByMaterialCategory ", produces = "application/json")
-    public List<CountByMaterialCategory> getCountByMaterialCategory() {
-
-        List<CountByMaterialCategory> cats = this.countbymaterialcategorydao.rawMaterialCountByMaterialCategory();
+    @GetMapping(path ="/countbymaterialcategory", produces = "application/json")
+    public List<CountByMaterialCategory> get() {
+        List<CountByMaterialCategory> cats =countbymaterialcategorydao.countCountByMaterialCategory();
+        System.out.println("in  --------"+cats);
         long totalCount = 0;
 
         for(CountByMaterialCategory countByMaterialCategory: cats){
@@ -33,7 +32,32 @@ public class ReportController {
         }
         System.out.println(cats);
         return cats;
+
     }
+
+    @Autowired
+    private CountByMaterialCategoryDao countbymaterialcategorydao;
+
+//    @GetMapping(path ="/countbymaterialcategory ", produces = "application/json")
+//    public List<CountByMaterialCategory> getCountByMaterialCategory() {
+//
+//        System.out.println("in  --------");
+//        List<CountByMaterialCategory> cats =countbymaterialcategorydao.countCountByMaterialCategory();
+//        long totalCount = 0;
+//
+//        for(CountByMaterialCategory countByMaterialCategory: cats){
+//            totalCount += countByMaterialCategory.getCount();
+//        }
+//
+//        for(CountByMaterialCategory countbymc: cats){
+//            long count = countbymc.getCount();
+//            double percentage = (double) count / totalCount * 100;
+//            percentage = Math.round(percentage*100.00)/100;
+//            countbymc.setPercentage(percentage);
+//        }
+//        System.out.println(cats);
+//        return cats;
+//    }
 
 }
 
--- ServerApp/src/main/java/lk/newdayproducts/report/dao/CountByMaterialCategoryDao.java
+++ ServerApp/src/main/java/lk/newdayproducts/report/dao/CountByMaterialCategoryDao.java
@@ -1,17 +1,14 @@
 package lk.newdayproducts.report.dao;
 
-import lk.newdayproducts.entity.Employee;
-import lk.newdayproducts.entity.Materialcategory;
+import lk.newdayproducts.report.entity.ClientCountByState;
 import lk.newdayproducts.report.entity.CountByMaterialCategory;
-import org.hibernate.sql.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CountByMaterialCategoryDao extends JpaRepository<CountByMaterialCategory, Integer> {
-
-   @Query("SELECT new CountByMaterialCategory(mc.name,count (*)) FROM Rawmaterial r,Materialcategory mc where r.materialcategory.id=mc.id group by mc.name")
-    List<CountByMaterialCategory> rawMaterialCountByMaterialCategory();
+        @Query
+        ("SELECT new CountByMaterialCategory(m.name, COUNT(*)) FROM Rawmaterial r, Materialcategory m WHERE m.id = r.materialcategory.id GROUP BY m.name")
+        List<CountByMaterialCategory> countCountByMaterialCategory();
 }
-
